163ac5de5c02f0a0792da5840837623c1256682d
zaenal <zaenal@laku6.com>
2020-04-18 16:22:08 +0700

    redirect to another activity

---
+++ Application/src/main/java/com/lk6/simpleworkprofile/AnotherActivity.java
@@ -0,0 +1,13 @@
+package com.lk6.simpleworkprofile;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AnotherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_another);
+    }
+}
